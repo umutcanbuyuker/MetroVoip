ef5309830ea2a26efa28db33dbec851cb92d99ba
umutcanbuyuker <can_umut_c@hotmail.com>
2024-09-12 09:54:13 +0300

    Add project files.

---
+++ MetroVoip.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MetroVoip.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Tram34-Voip/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tram34_Voip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
