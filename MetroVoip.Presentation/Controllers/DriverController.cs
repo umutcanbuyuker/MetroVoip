23f72723df94df2dd6b0678103c5846e27ceedd8
umutcanbuyuker <can_umut_c@hotmail.com>
2024-09-13 15:26:35 +0300

    Voip listener feature added.

--- MetroVoip.Presentation/Controllers/DriverController.cs
+++ MetroVoip.Presentation/Controllers/DriverController.cs
@@ -5,32 +5,15 @@ namespace MetroVoip.Presentation.Controllers
 {
     public class DriverController : Controller
     {
-        private readonly IIntercomService _intercomService;
         private readonly ICommunicationService _communicationService;
 
-        public DriverController(IIntercomService intercomService, ICommunicationService communicationService)
+        public DriverController(ICommunicationService communicationService)
         {
-            _intercomService = intercomService;
             _communicationService = communicationService;
         }
-
-        
-
         public async Task<IActionResult> Index()
         {
-            var intercoms = await _intercomService.GetAllIntercomsAsync();
-            return View(intercoms);
-        }
-        public async Task<IActionResult> SelectIntercom(int carriageNumber)
-        {
-            await _intercomService.StartCommunicationAsync(carriageNumber);
-            return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> StopIntercom(int carriageNumber)
-        {
-            await _intercomService.StopCommunicationAsync(carriageNumber);
-            return RedirectToAction("Index");
+            return View();
         }
 
         [HttpPost]
